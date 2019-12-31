6655da638f12da95307dbbd68d20f9583e4226a3
Rifqi Sambas Khairurrohman <rifqi@sera5.id>
2019-12-31 19:53:42 +0700

    add base footer

---
+++ src/common/footer/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "jquery";
+import "popper.js";
