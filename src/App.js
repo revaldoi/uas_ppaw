e6d68a298a77edf7598b129d336c22cfb99fddab
Rifqi Sambas Khairurrohman <rifqi@sera5.id>
2020-01-02 17:14:09 +0700

    fix some

--- src/App.js
+++ src/App.js
@@ -5,24 +5,10 @@ import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "jquery";
 import "popper.js";
-import Home from "./home/Home";
 import Catalog from "./catalog/Catalog";
-import Detail from "./detail/Detail";
 
 function App() {
-  return (
-    <Catalog />
-    // <HashRouter>
-    //     <div className="container-fluid text-center bg-dark">
-    //         <NavLink className="btn btn-danger" to="/Home">Home</NavLink>
-    //         <NavLink className="btn btn-primary" to="/catalog">Catalog</NavLink>
-    //         <NavLink className="btn btn-danger" to="/Detail">Detail</NavLink>
-    //     </div>
-    //     <Route exact="exact" path="/home" component={Home}/>
-    //     <Route exact="exact" path="/catalog" component={Catalog}/>
-    //     <Route exact="exact" path="/detail" component={Detail}/>
-    // </HashRouter>
-  );
+  return <Catalog />;
 }
 
 export default App;
--- src/catalog/Navbar.js
+++ src/catalog/Navbar.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import 'jquery';
-import 'popper.js';
-import './css/catalog.css';
-import NavbarDesktop from './navbar/Desktop';
-import NavbarMobile from './navbar/Mobile';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "jquery";
+import "popper.js";
+import NavbarDesktop from "./navbar/Desktop";
+import NavbarMobile from "./navbar/Mobile";
 
 function Navbar() {
-    return (
-        <div className="App">
-            <NavbarDesktop />
-            <NavbarMobile />
-        </div>
-    );
+  return (
+    <div className="App">
+      <NavbarDesktop />
+      <NavbarMobile />
+    </div>
+  );
 }
 
 export default Navbar;
